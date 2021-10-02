578436605982c583796f5bff4296cadc30b8983d
Josh Holmer <jholmer.in@gmail.com>
2021-10-01 21:15:41 -0400

    Add libdav1d support

--- build.rs
+++ build.rs
@@ -289,6 +289,7 @@ fn build() -> io::Result<()> {
     enable!(configure, "BUILD_LIB_AACPLUS", "libaacplus");
     enable!(configure, "BUILD_LIB_CELT", "libcelt");
     enable!(configure, "BUILD_LIB_DCADEC", "libdcadec");
+    enable!(configure, "BUILD_LIB_DAV1D", "libdav1d");
     enable!(configure, "BUILD_LIB_FAAC", "libfaac");
     enable!(configure, "BUILD_LIB_FDK_AAC", "libfdk-aac");
     enable!(configure, "BUILD_LIB_GSM", "libgsm");
