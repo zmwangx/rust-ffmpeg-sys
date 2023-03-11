5750e30625a5af3f7891c7f974db3be1d542ed54
AdrianEddy <adrian.eddy@gmail.com>
2023-03-11 21:11:18 +0100

    Update bindgen default enum style to allow for non-exhaustive enums

--- build.rs
+++ build.rs
@@ -1166,18 +1166,12 @@ fn main() {
         .blocklist_function("ynl")
         .opaque_type("__mingw_ldbl_type_t")
         .default_enum_style(bindgen::EnumVariation::Rust {
-            non_exhaustive: false,
+            non_exhaustive: env::var("CARGO_FEATURE_NON_EXHAUSTIVE_ENUMS").is_ok(),
         })
         .prepend_enum_name(false)
         .derive_eq(true)
         .size_t_is_usize(true)
         .parse_callbacks(Box::new(Callbacks));
-
-    if env::var("CARGO_FEATURE_NON_EXHAUSTIVE_ENUMS").is_ok() {
-        builder = builder.rustified_non_exhaustive_enum("*");
-    } else {
-        builder = builder.rustified_enum("*");
-    }
 
     // The input headers we would like to generate
     // bindings for.
