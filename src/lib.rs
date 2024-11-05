3bfbfc95a10e52508cc8ed81bc24c4addaad9955
Russell Greene <russell@shotover.com>
2024-11-04 21:23:17 -0700

    fmt & better comments

--- src/lib.rs
+++ src/lib.rs
@@ -17,23 +17,27 @@ extern crate ash;
 
 #[cfg(feature = "vulkan")]
 use ash::vk::{
-    Device as VkDevice, Format as VkFormat, Image as VkImage,
-    ImageCreateFlags as VkImageCreateFlags, ImageTiling as VkImageTiling,
-    ImageUsageFlags as VkImageUsageFlagBits, Instance as VkInstance, PFN_vkGetInstanceProcAddr,
-    PhysicalDevice as VkPhysicalDevice,
-    DeviceMemory as VkDeviceMemory,
-    MemoryPropertyFlags as VkMemoryPropertyFlagBits,
-    AccessFlags as VkAccessFlagBits,
-    ImageLayout as VkImageLayout,
-    Semaphore as VkSemaphore,
-    QueueFlags as VkQueueFlagBits,
-    VideoCodecOperationFlagsKHR as VkVideoCodecOperationFlagBitsKHR
+    AccessFlags as VkAccessFlagBits, Device as VkDevice, DeviceMemory as VkDeviceMemory,
+    Format as VkFormat, Image as VkImage, ImageCreateFlags as VkImageCreateFlags,
+    ImageLayout as VkImageLayout, ImageTiling as VkImageTiling,
+    ImageUsageFlags as VkImageUsageFlagBits, Instance as VkInstance,
+    MemoryPropertyFlags as VkMemoryPropertyFlagBits, PFN_vkGetInstanceProcAddr,
+    PhysicalDevice as VkPhysicalDevice, QueueFlags as VkQueueFlagBits, Semaphore as VkSemaphore,
+    VideoCodecOperationFlagsKHR as VkVideoCodecOperationFlagBitsKHR,
 };
 
+// the generated bindgen structs need these types that have lifetimes in them,
+// but there is no way within bindgen to propagate those lifetimes out into the structs
+// that contain these structs
+//
+// so, just put 'static to let it compile. Making sure the lifetimes are actually
+// check out nicely is now part of the checks an author must do when using the unsafe
+// functions that take in these structs or any other structs that contain them.
+
 #[cfg(feature = "vulkan")]
-type VkAllocationCallbacks = ash::vk::AllocationCallbacks<'static>; // hack!
+type VkAllocationCallbacks = ash::vk::AllocationCallbacks<'static>;
 #[cfg(feature = "vulkan")]
-type VkPhysicalDeviceFeatures2 = ash::vk::PhysicalDeviceFeatures2<'static>; // hack!
+type VkPhysicalDeviceFeatures2 = ash::vk::PhysicalDeviceFeatures2<'static>;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
